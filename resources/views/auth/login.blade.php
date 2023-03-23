1836f5cbf163012d64f0eb00b6c8c80d630a573d
Laurentiustd <laurenttius117@gmail.com>
2023-03-23 16:55:07 +0700

    create integration fe login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -45,3 +45,76 @@
         </div>
     </form>
 </x-guest-layout>
+ --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <form method="POST" action="{{ route('login') }}">
+            <div class="left-login">
+                @csrf
+                <div class="tittle">
+                    <h1>Welcome Back!</h1>
+                    <p>Let's log you back in!</p>
+                </div>
+                <div class="input">
+                    <label for="email" :value="__('Email')" style="color: #c9ada7">Email</label><br />
+                    <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                        required autofocus autocomplete="username" /><br />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <div class="password">
+                        <label for="password" :value="__('Password')" style="color: #c9ada7">Password</label><br />
+                        <input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="current-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                </div>
+                <div class="help">
+                    <div class="block mt-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox"
+                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                                name="remember">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+                    <div class="flex items-center justify-end mt-4">
+                        @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif
+                    </div>
+                </div>
+                <x-primary-button class="ml-3">
+                    {{ __('Sign In') }}
+                </x-primary-button>
+            </div>
+        </form>
+        <div class="right-login">
+            <div class="headings">
+                <h1>Hello, Friend!</h1>
+                <p>Fill up your information and start journey with us!</p>
+                <button><a href="/register" style="text-decoration: none; color: inherit">Sign Up</a></button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
