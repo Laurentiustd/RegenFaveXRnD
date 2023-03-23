0265f782533737b336ebe5f3d5a6f98d8df01edc
Clarice-Wijaya <114148993+Clarice-Wijaya@users.noreply.github.com>
2023-03-23 23:44:52 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,44 +5,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=PT Serif Caption' rel='stylesheet'>
     <title>Home</title>
 </head>
 <body>
     <!-- navbar -->
-    <nav class=" px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0  border-gray-200 dark:border-gray-600">
-        <div class="container flex flex-wrap items-center justify-between mx-auto">
+    <nav class="py-2.5">
+        <div class="flex flex-wrap items-center justify-around px-4 mx-10">
             <a href="" class="flex items-center">
-                <span class="self-center text-xl font-normal whitespace-nowrap dark:text-white">BrainGrow</span>
+                <span id="brand" class="self-center text-2xl whitespace-nowrap dark:text-white">BrainGrow</span>
             </a>
             
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto" >
+                <ul class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-white text-base font-semibold bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 text-base font-semibold hover:text-blue-700 md:p-0 ">About</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Course</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 text-base font-semibold rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Course</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 text-base font-semibold rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 text-base font-semibold  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                 </li>
                 </ul>
             </div>
-            <div class="flex md:order-2">
-                <a  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Log In</a>
-                <a href="/step1" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Sign Up</a>
+            <div class="flex items-center">
+                <a id="loginbtn" class="hover:bg-slate-700 hover:text-rose-50 font-semibold pt-1.5 pb-2 px-9 mx-2 rounded-full">Log In</a>
+                <a id="signupbtn" href="/step1" class="hover:bg-blue-700 font-semibold pt-2 pb-2.5 px-9 mx-2 rounded-full">Sign Up</a>
                 
             </div>
         </div>
         
     </nav>
+
+    <section class=" dark:bg-gray-900">
+        <div class="flex justify-evenly left-0 mx-28 px-4 pt-20 pb-8 ">
+            <div class="self-center">
+                <h1 class="mb-10 text-6xl font-medium">Investing in
+                    <br><strong class="font-extrabold">Knowledge</strong> and 
+                    <br><strong class="font-extrabold">Your Future</strong>
+                </h1>
+                
+                <div class="space-y-4">
+                    <a id="joinbtn" href="/" class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg sm:w-auto "></a>
+                </div>
+            </div>
+            <div id="saly" class="mt-20">
+                <img src="{{ asset('assets/Saly-10.png') }}" alt="">
+            </div>                
+        </div>
+    </section>
 </body>
 </html>
